f47eb4aaf7355d9774a197c0a980d670da96498d
Vlad <shevchenkovlad777@gmail.com>
2016-06-22 19:11:01 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by vlad on 20.05.16.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String APP_ID = "5470811" ;
+        VkApi.with(APP_ID, null);
+
+
+    }
+
+}
